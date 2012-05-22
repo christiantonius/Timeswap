8ec45ed041ff26caca6ccc928ae84861c1f68f25
Antonius Christian <kodokisfrog1@gmail.com>
2012-05-22 19:16:02 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TimeswapRails::Application.config.secret_token = '0ee361a0a9bbb69ec5e2c2c8bd5627638a18a6d5d0cdd850a04fb143ffcec3a615d1cccb51898f798a19ca1fdd8c0ed3418062d4ce8abde3ec1d3aa43139dbe1'
